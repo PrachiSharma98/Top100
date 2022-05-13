345f9e5b928108e40bfbaa6be4518c27c6dbf6dd
PrachiSharma98 <99522178+PrachiSharma98@users.noreply.github.com>
2022-05-13 14:33:27 +0530

    Create CodingQues.java

---
+++ CodingQues.java
@@ -0,0 +1,190 @@
+Oracle
+************
+Easy:-
+https://leetcode.com/problems/convert-binary-number-in-a-linked-list-to-integer/
+1636	-Sort Array by Increasing Frequency
+509	-Fibonacci Number
+108	-Convert Sorted Array to Binary Search Tree
+706	-Design HashMap
+696	-Count Binary Substrings
+191	-Number of 1 Bits
+Merge Two Sorted Lists
+Roman to Integer
+First Unique Character in a String
+Best Time to Buy and Sell Stock
+Add Strings
+Valid Parentheses
+Merge Sorted Array
+Max Area of Island
+Generate Parentheses
+Number of Islands
+	7	
+Reverse Integer	26.7%	Medium	
+759	
+Employee Free Time
+71.1%	Hard	
+42	
+Trapping Rain Water	56.6%	Hard	
+297	
+Serialize and Deserialize Binary Tree	53.8%	Hard	
+295	
+Find Median from Data Stream	50.4%	Hard	
+1326	
+Minimum Number of Taps to Open to Water a Garden	48.1%	Hard	
+23	
+Merge k Sorted Lists	47.1%	Hard	
+715	
+Range Module	43.6%	Hard	
+460	
+LFU Cache	39.4%	Hard	
+76	
+Minimum Window Substring	39.0%	Hard	
+124	
+Binary Tree Maximum Path Sum	37.7%	Hard	
+32	
+Longest Valid Parentheses	31.3%	Hard	
+30	
+Substring with Concatenation of All Words	28.2%	Hard	
+149	
+Max Points on a Line
+
+	1	
+Two Sum	48.6%	Easy	
+141	
+Linked List Cycle	45.9%	Easy	
+112	
+Path Sum	45.6%	Easy	
+88	
+Merge Sorted Array	43.9%	Easy	
+20	
+Valid Parentheses	40.9%	Easy	
+680	
+Valid Palindrome II	39.4%	Easy	
+1762	
+Buildings With an Ocean View
+79.8%	Medium	
+1472	
+Design Browser History	75.0%	Medium	
+22	
+Generate Parentheses	70.2%	Medium	
+695	
+Max Area of Island	69.8%	Medium	
+1268	
+Search Suggestions System	65.3%	Medium	
+347	
+Top K Frequent Elements	65.0%	Medium	
+49	
+Group Anagrams	64.3%	Medium	
+238	
+Product of Array Except Self	64.0%	Medium	
+215	
+Kth Largest Element in an Array	63.6%	Medium	
+1344	
+Angle Between Hands of a Clock	63.3%	Medium	
+122	
+Best Time to Buy and Sell Stock II	62.1%	Medium	
+102	
+Binary Tree Level Order Traversal	61.0%	Medium	
+199	
+Binary Tree Right Side View	59.6%	Medium	
+64	
+Minimum Path Sum	59.4%	Medium	
+516	
+Longest Palindromic Subsequence	59.4%	Medium	
+430	
+Flatten a Multilevel Doubly Linked List	58.9%	Medium	
+445	
+Add Two Numbers II	58.7%	Medium	
+208	
+Implement Trie (Prefix Tree)	58.3%	Medium	
+1048	
+Longest String Chain	57.9%	Medium	
+394	
+Decode String	56.4%	Medium	
+236	
+Lowest Common Ancestor of a Binary Tree	55.5%	Medium	
+1229	
+Meeting Scheduler
+54.8%	Medium	
+17	
+Letter Combinations of a Phone Number	54.3%	Medium	
+113	
+Path Sum II	54.1%	Medium	
+200	
+Number of Islands	54.0%	Medium	
+103	
+Binary Tree Zigzag Level Order Traversal	53.7%	Medium	
+1010	
+Pairs of Songs With Total Durations Divisible by 60	53.6%	Medium	
+934	
+Shortest Bridge	52.8%	Medium	
+981	
+Time Based Key-Value Store	52.7%	Medium	
+40	
+Combination Sum II	52.4%	Medium	
+767	
+Reorganize String	52.1%	Medium	
+380	
+Insert Delete GetRandom O(1)	51.4%	Medium	
+253	
+Meeting Rooms II
+49.7%	Medium	
+450	
+Delete Node in a BST	49.1%	Medium	
+240	
+Search a 2D Matrix II	48.5%	Medium	
+438	
+Find All Anagrams in a String	48.3%	Medium	
+153	
+Find Minimum in Rotated Sorted Array	48.0%	Medium	
+848	
+Shifting Letters	45.4%	Medium	
+207	
+Course Schedule	45.1%	Medium	
+56	
+Merge Intervals	45.0%	Medium	
+567	
+Permutation in String	44.7%	Medium	
+139	
+Word Break	44.6%	Medium	
+658	
+Find K Closest Elements	44.6%	Medium	
+74	
+Search a 2D Matrix	44.5%	Medium	
+735	
+Asteroid Collision	44.4%	Medium	
+560	
+Subarray Sum Equals K	44.2%	Medium	
+150	
+Evaluate Reverse Polish Notation	42.7%	Medium	
+227	
+Basic Calculator II	41.6%	Medium	
+333	
+Largest BST Subtree
+41.3%	Medium	
+146	
+LRU Cache	39.9%	Medium	
+79	
+Word Search	39.7%	Medium	
+2	
+Add Two Numbers	38.7%	Medium	
+33	
+Search in Rotated Sorted Array	37.8%	Medium	
+55	
+Jump Game	37.8%	Medium	
+63	
+Unique Paths II	37.6%	Medium	
+31	
+Next Permutation	36.5%	Medium	
+3	
+Longest Substring Without Repeating Characters	33.1%	Medium	
+179	
+Largest Number	33.0%	Medium	
+50	
+Pow(x, n)	32.4%	Medium	
+5	
+Longest Palindromic Substring	31.8%	Medium	
+15	
+3Sum	31.1%	Medium	
+98	
+Validate Binary Search Tree
